d2167003331cc2d9beda7012b71972dd5ce8da1e
DanielStoev <dannybob63@gmail.com>
2024-05-29 23:58:31 +0300

    Added logic

--- cats-vs-dogs/src/app/app.component.ts
+++ cats-vs-dogs/src/app/app.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { CatService } from './cat/cat.service';
+import { DogService } from './dog/dog.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, HttpClientModule],
+  providers: [CatService, DogService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cats-vs-dogs';
+  title: string = 'cats-vs-dogs';
+
+  catFact!: string;
+  catImage!: string;
+  dogFact!: string;
+  dogImage!: string;
+
+  constructor(private catService: CatService, private dogService: DogService) {}
+
+  showCat(): void {
+    this.catService.getCatFact().subscribe((data: any) => {
+      this.catFact = data.data[0];
+    });
+
+    this.catService.getCatImage().subscribe((data: any) => {
+      this.catImage = data[0].url;
+    });
+  }
+
+  showDog(): void {
+    this.dogService.getDogFact().subscribe((data: any) => {
+      this.dogFact = data.facts[0];
+    });
+
+    this.dogService.getDogImage().subscribe((data: any) => {
+      this.dogImage = data.message;
+    });
+  }
 }
--- cats-vs-dogs/src/app/dog/dog.component.ts
+++ cats-vs-dogs/src/app/dog/dog.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Component({
-  selector: 'app-dog',
-  standalone: true,
-  imports: [],
-  templateUrl: './dog.component.html',
-  styleUrl: './dog.component.css'
+@Injectable({
+  providedIn: 'root',
 })
-export class DogComponent {
+export class DogService {
+  private dogFactUrl = 'https://dog-api.kinduff.com/api/facts';
+  private dogImageUrl = 'https://dog.ceo/api/breeds/image/random';
 
+  constructor(private http: HttpClient) {}
+
+  getDogFact(): Observable<any> {
+    return this.http.get(this.dogFactUrl);
+  }
+
+  getDogImage(): Observable<any> {
+    return this.http.get(this.dogImageUrl);
+  }
 }
